dcd497c51a4ec7ef3cbe4810e2dc004deba30e06
sbanoseg <97255567+sbanoseg@users.noreply.github.com>
2022-08-29 13:30:46 -0500

    25th commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ console.log('mi nombre es robot');
 console.log('y quiero platicar contigo');
 console.log('cual es tu nombre?');
 console.log('cual es tu edad?');
+console.log('cual es tu p apellido?');
